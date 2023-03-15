a57e5bac07d02684e7608288c4836d46ab1c58d1
sosauce <98750531+sosauce@users.noreply.github.com>
2023-03-15 19:00:59 +0100

    Add files via upload

---
+++ scriptPass.js
@@ -0,0 +1,10 @@
+function sendEmail() {
+    var email = document.getElementById("email-input").value;
+    
+    // Send reset password email
+    // ...
+  
+    var messageBubble = document.getElementById("message-bubble");
+    messageBubble.style.display = "block";
+  }
+  
